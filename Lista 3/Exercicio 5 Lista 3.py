d5a1f46c7488bc45057023891d31b3b1a2d1b244
Templasan <163298566+Templasan@users.noreply.github.com>
2025-03-22 11:41:54 -0300

    Update Exercicio 5 Lista 3.py

--- Lista 3/Exercicio 5 Lista 3.py
+++ Lista 3/Exercicio 5 Lista 3.py
@@ -1,6 +1,15 @@
-A = int(input())
-B = int(input())
-while B != 0 || A != 0:
-    r = A
-    
+A = int(input('Numero 1: '))
+B = int(input('Numero 2: '))
+
+if a == 0 or b == 0:
+    print('Numero não pode ser igual a 0')
+else:
+    if b > a:
+        a, b = b, a
+    while b != 0:
+        c = a
+        a = b
+        b = c
+        b = c % b
+    print(a,'É o MDC')
     
